4934777e9b4ac788678d1699f5079d8f0113ddaf
ArcaneCipher <desjardins.jarp@gmail.com>
2025-01-01 23:27:26 -0500

    Add tree functions to determin vampire lineage for app

--- vampire.js
+++ vampire.js
@@ -10,22 +10,39 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    // Add the given vampire to the offspring array of this vampire
+    this.offspring.push(vampire);
 
+    // Set the creator of the given vampire to this vampire
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    // Return the length of the offspring array, which represents the count of vampires created
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0; // Initialize the count of generations from the original vampire
+    let currentVampire = this; // Start with the current vampire instance
 
+    // Traverse up the lineage until the original vampire (creator is null) is reached
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator; // Move up to the creator (parent vampire)
+      numberOfVampires++; // Increment the count of generations
+    }
+
+    // Return the total number of generations from the original vampire
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    // Compare the number of generations from the original vampire
+    // Return true if this vampire is closer to the original vampire
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,7 +53,27 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    // Create a set to store the current vampire's ancestors
+    const ancestors = new Set();
+    
+    // Add this vampire's ancestors to the set
+    let currentVampire = this;
+    while (currentVampire) {
+      ancestors.add(currentVampire);
+      currentVampire = currentVampire.creator;
+    }
+    
+    // Traverse the other vampire's lineage and find the first common ancestor
+    currentVampire = vampire;
+    while (currentVampire) {
+      if (ancestors.has(currentVampire)) {
+        return currentVampire;
+      }
+      currentVampire = currentVampire.creator;
+    }
+    
+    // If no common ancestor is found (shouldn't happen in a valid tree), return null
+    return null;
   }
 }
 
